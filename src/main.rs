969e7e5cf4604a861c81fc3cbca96752262435ca
Rayssa <guimaraesr305@gmail.com>
2024-07-29 21:14:00 -0300

    adding view tasks and removing tasks

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ use clap::Parser;
 fn main() {
    let args = todo_list::Args::parse();
    todo_list::create_new_task(&args).expect("Erro ao criar nova task.");
+   todo_list::view_tasks(&args).expect("Não foi possível visualizar tasks");
+   todo_list::remove_task(&args).expect("Não foi possível remover tasks.");
 }
--- src/todo_list.rs
+++ src/todo_list.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use serde::{Serialize, Deserialize};
 use serde_json;
 use std::fs;
+use std::io::{self, Read, Write};
 use colored::*;
 
 #[derive(Debug, Parser)]
@@ -31,22 +32,69 @@ struct NewTask {
     completion_time: String,
 }
 
-pub fn create_new_task(args: &Args) -> std::io::Result<()>{
-    if let Some(task) = &args.add_task{
+pub fn create_new_task(args: &Args) -> std::io::Result<()>{    if let Some(task) = &args.add_task{
         if task.len() > 1{
-            let data = NewTask{
+            let new_task = NewTask{
                 name: task[0].to_owned(),
                 completion_time: task[1].to_owned(),
             };
-            let data_serialized = serde_json::to_string_pretty(&data)?;
-            fs::write("src/serde.json", data_serialized)?;
+
+            let mut tasks: Vec<NewTask> = if let Ok(mut file) = fs::File::open("src/serde.json"){
+                let mut data = String::new();
+                file.read_to_string(&mut data)?;
+                serde_json::from_str(&data).unwrap_or_else(|_| vec![]) //essa casinha é um closure, significa: se houver um erro ignore e retorne um vetor vazio
+            } else {
+                vec![]
+            };
+
+            tasks.push(new_task);
+
+            let data_serialized = serde_json::to_string_pretty(&tasks)?;
+            let mut file = fs::File::create("src/serde.json")?;
+            file.write_all(data_serialized.as_bytes())?;
             println!("{}", format!("Task adcionada com sucesso.").green())
         } else {
             println!("{}", format!("É necessário passar dois argumentos.").red())
         }
-    } else {
-        println!("{}", format!("Nenhum argumento foi passado").red());
+    } 
+    Ok(())
+}
+
+pub fn view_tasks(args: &Args) -> std::io::Result<()>{
+    if let Some(view) =  &args.view_tasks{
+        if *view {
+            let mut json_file = fs::File::open("src/serde.json")?;
+            let mut data = String::new();
+            json_file.read_to_string(&mut data)?;
+            let json_data: Vec<NewTask> = serde_json::from_str(&data).expect("Erro ao desserializar a data.");
+
+            println!("{}", format!("\n--------------------------\nTasks para concluir: \n--------------------------").bright_red());
+            for (i, task) in json_data.iter().enumerate() {
+                println!("{}", format!("#{}º", i + 1).white());
+                println!("{}", format!("Nome: {}", task.name).green());
+                println!("{}", format!("Prazo de conclusão: {}\n", task.completion_time).green());
+            }
+        }
     }
+    Ok(())
+}
+
+pub fn remove_task(args: &Args) -> std::io::Result<()> {
+    if let Some(task_to_remove) = &args.remove_task{
+        let mut json_file = fs::File::open("src/serde.json")?;
+        let mut data = String::new();
+        json_file.read_to_string(&mut data)?;
+        let mut json_data: Vec<NewTask> = serde_json::from_str(&data).expect("Erro ao deserializar os dados para remover.");
+
+        json_data.retain(|task| task.name != *task_to_remove); //retain filtra elementos que possuem determinada condicao, |item| condiiton
+
+        let data_serialized = serde_json::to_string_pretty(&json_data)?;
+        let mut file = fs::File::create("src/serde.json")?;
+        file.write_all(data_serialized.as_bytes())?;
+
+
+        println!("{}", format!("Tarefa removida com sucesso.").green());
+    } 
     Ok(())
 }
 
